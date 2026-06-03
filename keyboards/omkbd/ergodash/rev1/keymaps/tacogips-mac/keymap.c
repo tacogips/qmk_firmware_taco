1ea72944193ad84bad9d6ace1ffd78c5ddfec03a
tacogips <me@tacogips.me>
2026-06-03 17:32:53 +0900

    Update ErgoDash mac thumb keys

--- keyboards/omkbd/ergodash/rev1/keymaps/tacogips-mac/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/tacogips-mac/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | }    | Sfhit|  Z  |   X  |   C  |   V   |  B   |                   |  %   | N  |   M  |   ,  |   .  |   /     |  Up  |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Enter| BS |  Del |  ALt |||||||| Shift| Ctrl |Space |||||||| Alt  | Shift | Space|||||||| Ctrl | Left | Down | Right|
+   * | Enter| BS |  Del | Shift|||||||| Space| Ctrl | GUI  |||||||| Alt  | Shift | Space|||||||| Ctrl | Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT(
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,     KC_W,     KC_E,   KC_R,  KC_T,    KC_GRV,                  KC_QUOT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC ,       \
     KC_LGUI, KC_A,     KC_S,     KC_D,   KC_F,  KC_G,    KC_ESC,                  KC_ASTR, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RBRC,   \
     KC_RCBR, KC_LSFT,  KC_Z,     KC_X,   KC_C,  KC_V,    KC_B,                    KC_PERC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,       \
-    KC_ENT,  KC_BSPC,  KC_DEL,  KC_LGUI,  KC_LSFT, KC_LCTL,KC_SPC,        KC_LALT, KC_LSFT, KC_SPC, KC_LCTL,     KC_LEFT, KC_DOWN,  KC_RGHT \
+    KC_ENT,  KC_BSPC,  KC_DEL,  KC_LSFT,  KC_SPC, KC_LCTL,KC_LGUI,        KC_LALT, KC_LSFT, KC_SPC, KC_LCTL,     KC_LEFT, KC_DOWN,  KC_RGHT \
   ),
 
 };
